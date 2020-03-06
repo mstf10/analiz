4f272fc94f2951b9e8bb70500a24a9ef5a78447b
mstf10 <m.erguven10@outlook.com>
2020-03-06 17:46:30 +0300

    Analiz

--- src/TahakkukAnalizPackage/TahakkukAnaliz.java
+++ src/TahakkukAnalizPackage/TahakkukAnaliz.java
@@ -2,8 +2,6 @@ package TahakkukAnalizPackage;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class TahakkukAnaliz {
     private JFrame jFrame_TahakkukAnaliz;
@@ -22,31 +20,32 @@ public class TahakkukAnaliz {
         jFrame_TahakkukAnaliz = new JFrame("Tahakkuk Analiz");
         jFrame_TahakkukAnaliz.setVisible(true);
         jFrame_TahakkukAnaliz.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame_TahakkukAnaliz.setSize(1650, 800);
+        jFrame_TahakkukAnaliz.setSize(1700, 800);
         jFrame_TahakkukAnaliz.setLayout(null);
     }
 
-    private void setjTabbedPane(){
+    private void setjTabbedPane() {
         jTabbedPane = new JTabbedPane();
-        jTabbedPane.setBounds(0,0,1650,600);
+        jTabbedPane.setBounds(0, 0, 1700, 800);
         jPanel_anapanel.add(jTabbedPane);
-       jTabbedPane.addTab("Gelen Ödenek", new GelenÖdenek().jPanel_gelenödenek);
-       jTabbedPane.addTab("Hizmet Harcama", new HizmetHarcama().jPanel_hizmetharcama);
+        jTabbedPane.addTab("ÖDENEK ve FATURA", new GelenÖdenek().jPanel_gelenödenek);
+        jTabbedPane.addTab("TDMS - BL1 (2020)", new TdmsBL1_2020().jPanel_TdmsBL1_2020);
+        jTabbedPane.addTab("TDMS - BL1 (2019)", new TdmsBL1_2019().jPanel_TdmsBL1_2019);
+        jTabbedPane.addTab("BORÇ DAĞILIMI",new BorçDağılım().jPanel_borçdağılım);
 
     }
 
-    private void setjPanel_anapanel(){
+    private void setjPanel_anapanel() {
         jPanel_anapanel = new JPanel();
         jPanel_anapanel.setLayout(null);
-        jPanel_anapanel.setBounds(0,0,1650,800);
-        jPanel_anapanel.setBackground(Color.yellow);
+        jPanel_anapanel.setBounds(0, 0, 1700, 800);
+        jPanel_anapanel.setBackground(Color.YELLOW);
         jFrame_TahakkukAnaliz.getContentPane().add(jPanel_anapanel);
-
-
     }
 
 
     public static void main(String[] args) {
         TahakkukAnaliz tahakkukAnaliz = new TahakkukAnaliz();
+
     }
 }
